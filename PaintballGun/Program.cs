de40644abf6ab1b75bfd20691bf083121290e3ae
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-26 11:15:20 +0200

    Added Main method

--- PaintballGun/Program.cs
+++ PaintballGun/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿PaintballGun.PaintballGun gun = new PaintballGun.PaintballGun();
+
+while (true)
+{
+    Console.WriteLine($"Kulki: {gun.GetBalls()}, załadowano: {gun.GetBallsLoaded()}");
+    if (gun.IsEmpty()) Console.WriteLine("OSTRZEŻENIE: brak amunicji");
+    Console.WriteLine("[Spacja] - strzał, [p] - przeładowanie, " + "[+] - dodaj amunicje, [k] - koniec");
+    char key = Console.ReadKey(true).KeyChar;
+    if (key == ' ') Console.WriteLine($"Próba strzału: {gun.Shoot()}");
+    else if (key == 'p') gun.Reload();
+    else if (key == '+') gun.SetBalls(gun.GetBalls() + PaintballGun.PaintballGun.MAGAZINE_SIZE);
+    else if (key == 'k') return;
+}
